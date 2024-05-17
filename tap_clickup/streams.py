53f48cd128a379e931868df7d0c7a4ef8f16e3e2
Mason <masonlopez@damotech.com>
2024-05-17 15:31:26 -0400

    Bring back replication key usuage for time entries

--- tap_clickup/streams.py
+++ tap_clickup/streams.py
@@ -40,7 +40,6 @@ class TimeEntries(ClickUpStream):
     schema_filepath = SCHEMAS_DIR / "time_entries.json"
     records_jsonpath = "$.data[*]"
     parent_stream_type = TeamsStream
-
     # TODO not clear why this is needed
     partitions = None
 
@@ -50,19 +49,14 @@ class TimeEntries(ClickUpStream):
         """Return a dictionary of values to be used in URL parameterization."""
         params = super().get_url_params(context, next_page_token)
 
-        # if "time_entry_start_date" in self.config:
-        # Formatted in ISO 8601, it must now be converted to milliseconds
-        # start_date = datetime.strptime(self.config["time_entry_start_date"], "%Y-%m-%dT%H:%M:%SZ")
         unformatted_state_date = self.get_starting_replication_key_value(context)
         # Convert the datetime object to milliseconds
         if not unformatted_state_date:
-            # print("No start date or state date fallback to config date")
             start_date = datetime.strptime(self.config["time_entry_start_date"], "%Y-%m-%dT%H:%M:%SZ")
             params["start_date"] = int(start_date.timestamp() * 1000)
-            # print("Start date: ", params["start_date"])
-        # else:
-        # Because the state date is already in milliseconds, we can just use it
-        # params["start_date"] = unformatted_state_date
+        else:
+            # Because the state date is already in milliseconds, we can just use it
+            params["start_date"] = unformatted_state_date
         if "time_entry_assignees" in self.config:
             params["assignee"] = self.config["time_entry_assignees"]
         else:
